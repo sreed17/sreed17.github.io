40ac0b81f74604b68789e9dfb6915ec5f1c1cadd
SReED17 <inbox.sreekanthd@gmail.com>
2023-04-01 15:08:11 +0530

    add resposive layout

--- src/App.jsx
+++ src/App.jsx
@@ -21,62 +21,62 @@ function App() {
 
   return (
     <>
-      <section
-        id="hero"
-        className="md:container h-[100svh] flex flex-col justify-evenly items-center "
-      >
-        <hgroup className="grid grid-cols-2">
-          <h1 className=" col-span-2 place-self-center text-[5rem] ">
+      <section id="hero">
+        <hgroup className="grid grid-flow-row place-content-center ">
+          <h1 className="text-[3rem] font-bold place-self-center md:text-[5rem] md:col-span-2">
             Sreekanth D
           </h1>
-          <h2 className="text-base">Digital Craftsman</h2>
-          <h3 className="place-self-end">Artist/Designer/Developer</h3>
+          <h2 className="text-xl font-semibold place-self-center md:place-self-start md:row-start-2">
+            Digital Craftsman
+          </h2>
+          <h3 className="text-sm p-1 place-self-center md:place-self-end md:row-start-2">
+            Artist / Designer / Developer
+          </h3>
         </hgroup>
-        <div className="max-w-24 grid grid-cols-2 gap-2   ">
-          <p className="text-2xl max-w-[38ch] place-content-end">
+        <div className="grid grid-flow-row gap-8 px-8">
+          <p className="text-xl md:max-w-[30ch] md:text-3xl">
             I will help you turn your ideas into a functional and aesthetically
             pleasing experience.
           </p>
-          <div className="flex flex-col gap-1 place-self-center ">
-            <span className="text-xl font-bold text-violet-500">
+          <div className="flex flex-col w-full md:col-start-2">
+            <span className=" text-violet-400 md:text-xl">
               Available for freelance work
             </span>
-            <span className="text-2xl font-semibold">
+            <span className="font-bold text-lg md:text-2xl md:font-semibold">
               inbox.sreekanthd@gmail.com
             </span>
             <ul className="flex flex-row gap-2">
               <li>
-                <a href="">Github</a>
+                <a href="https://github.com/sreed17" className="social-link">
+                  Github
+                </a>
               </li>
               <li className="text-violet-500">•</li>
               <li>
-                <a>LinkedIn</a>
+                <a
+                  href="https://in.linkedin.com/in/sreekanth-d-787726b8"
+                  className="social-link"
+                >
+                  LinkedIn
+                </a>
               </li>
             </ul>
           </div>
-          <nav className="flex flex-row gap-4">
-            <a
-              href="#portfolio"
-              className=" box-border w-48 h-16 bg-violet-500 flex flex-row justify-center items-center gap-2 text-md font-bold"
-            >
-              See my works <IoArrowDownCircleOutline size={24} />
-            </a>
-            <a
-              href="#about-me"
-              className=" box-border w-48 h-16 border-solid border-violet-500 border-2 text-violet-500 flex flex-row justify-center items-center gap-2 text-md font-bold"
-            >
-              More about me <IoArrowDownCircleOutline size={24} />
-            </a>
-          </nav>
         </div>
+        <nav className="flex flex-row gap-4 items-center justify-center flex-wrap">
+          <a href="#portfolio" className="btn-fill">
+            See my works <IoArrowDownCircleOutline size={24} />
+          </a>
+          <a href="#about-me" className=" btn-outline">
+            More about me <IoArrowDownCircleOutline size={24} />
+          </a>
+        </nav>
       </section>
-      <section
-        id="about-me"
-        className="md:container h-[100svh] flex flex-col justify-evenly items-center "
-      >
-        <div className="grid grid-cols-2 grid-rows-2">
-          <h1 className="section-title">ABOUT ME</h1>
-          <p className="w-[56ch] row-start-2 first-letter:text-2xl first-letter:text-violet-500">
+      <section id="about-me" className="px-8 gap-8">
+        <div className="grid grid-flow-row gap-8 md:gap-x-32 place-content-center">
+          <h1 className="section-title">About Me</h1>
+
+          <p className="text-sm md:text-lg md:row-start-2 md:col-start-1 md:w-[60ch]">
             I am a self taught artist, designer and developer based in Kerala,
             India. I am a Mathematics post graduate and Computer science
             researcher who has great passion for art and engineer. I like using
@@ -88,12 +88,14 @@ function App() {
             src="https://images.unsplash.com/photo-1640952131659-49a06dd90ad2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
             height={64}
             width={64}
-            className="w-64 h-64 aspect-square object-cover rounded-full row-span-2 place-self-end"
+            className=" row-start-2 w-48 h-48 md:w-64 md:h-64 aspect-square object-cover rounded-full place-self-center md:row-span-2 md:col-start-2"
           />
         </div>
-        <div className=" md:container md:px-24 flex flex-col p-4 gap-4">
-          <h1 className="">Why work with me?</h1>
-          <ul className="reasons  flex flex-row gap-4">
+        <div className="w-full mb-16 flex flex-col items-center justify-center">
+          <h1 className="text-xl md:text-3xl text-center p-4 font-bold ">
+            Why work with me?
+          </h1>
+          <ul className="reasons  flex flex-row flex-wrap gap-4">
             <li>
               <h2>Flexible & Experienced</h2>
               <p>
@@ -109,19 +111,12 @@ function App() {
                 money back.
               </p>
             </li>
-            <li>
-              <h2>Extension of your team</h2>
-              <p>I know that we get the best results when combine forces.</p>
-            </li>
           </ul>
         </div>
       </section>
-      <section
-        id="portfolio"
-        className="w-full min-h-[100svh] flex flex-col justify-start items-center  "
-      >
-        <header className="pt-4 md:container sticky top-0 dark:bg-black border-solid dark:border-slate-900 border-b-2">
-          <h1 className="section-title">Portfolio</h1>
+      <section id="portfolio" className="justify-start">
+        <header className="w-full sticky top-0 px-8 flex flex-row justify-center">
+          <h1 className="section-title md:w-[65rem]">Portfolio</h1>
         </header>
         <div className=" w-full h-fit p-2"></div>
       </section>
